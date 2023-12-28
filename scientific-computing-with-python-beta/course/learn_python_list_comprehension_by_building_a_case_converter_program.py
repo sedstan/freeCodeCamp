e93438306be64002b90732c5b8a30f1da663530c
Sedky Said <sed@sedkystanleysaid.com>
2023-12-28 15:16:13 +0000

    adds list comprehension lesson

---
+++ scientific-computing-with-python-beta/course/learn_python_list_comprehension_by_building_a_case_converter_program.py
@@ -0,0 +1,35 @@
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    """Converts string to snake_case
+    
+        :params pascal_or_camel_cased_string: string - a string to convert into snake_case
+        :returns snake_cased_char_list: list - new string in the format of snake_case"""
+    # Standard way to convert
+    # snake_cased_char_list = []
+    # for char in pascal_or_camel_cased_string:
+    #     if char.isupper():
+    #         converted_character = '_' + char.lower()
+    #         snake_cased_char_list.append(converted_character)
+    #     else:
+    #         snake_cased_char_list.append(char)
+    # snake_cased_string = ''.join(snake_cased_char_list)
+    # clean_snake_cased_string = snake_cased_string.strip('_')
+
+    # return clean_snake_cased_string
+
+    #  List comprehension way to convert
+    snake_cased_char_list = [
+        '_' + char.lower() if char.isupper()
+        else char
+        for char in pascal_or_camel_cased_string
+    ]
+
+    return ''.join(snake_cased_char_list).strip('_')
+
+
+def main():
+    # print(convert_to_snake_case('aLongAndComplexString')) # case 1
+    print(convert_to_snake_case('IAmAPascalCasedString')) # case 2
+
+
+if __name__ == '__main__':
+    main()
