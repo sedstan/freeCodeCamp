181522f944e2a2e24ef07fc350f6d36ac81b416f
Sedky Said <sed@sedkystanleysaid.com>
2024-01-08 11:12:39 +0000

    Change `unvisited` --import heap library --replace `unvisited` with `heap` to make it more effcient.

--- scientific-computing-with-python-beta/course/learn_algorithm_design_by_building_a_shortest_path_algorithm(refactor).py
+++ scientific-computing-with-python-beta/course/learn_algorithm_design_by_building_a_shortest_path_algorithm(refactor).py
@@ -1,3 +1,5 @@
+import heapq
+
 my_graph = {
     'A': [('B', 5), ('C', 3), ('E', 11)],
     'B': [('A', 5), ('C', 1), ('F', 2)],
@@ -13,20 +15,28 @@ def shortest_path(graph, start, target=''):
         :params: graph: dictionary - a dictionary of nodes that lists the next corresponding node and its distance
         :params: start: string - the key of the key:value pair in the graph
         :return distances, paths: dictionary - the dictionaries of the distances and paths for each node  """
-    unvisited = list(graph)
+    heap = [(0, start)] #start with the start node with a distance of 0
+    visited = set()
     distances = {node: 0 if node == start else float('inf') for node in graph}
     paths = {node: [] for node in graph}
     paths[start].append(start)
 
-    while unvisited:
-        current = min(unvisited, key=distances.get)
+    while heap:
+        current_distance, current = heapq.heappop(heap) #get node with smallest distance
+        if current in visited:
+            continue #skip if already visited
+        visited.add(current)
+
         for node, distance in graph[current]:
-            if distance + distances[current] < distances[node]:
-                distances[node] = distance + distances[current]
-                # check if shorter path exsits and updates path.
-                paths[node] = paths[current][:]
-                paths[node].append(node)
-        unvisited.remove(current)
+            # if distance + distances[current] < distances[node]:
+            if node not in visited:
+                new_distance = current_distance + distance
+                if new_distance < distances[node]:
+                    distances[node] = new_distance
+                    # check if shorter path exsits and updates path.
+                    paths[node] = paths[current][:]
+                    paths[node].append(node)
+                    heapq.heappush(heap,(new_distance, node)) #add or update node in heap
 
     targets_to_print = [target] if target else graph
     for node in targets_to_print:
